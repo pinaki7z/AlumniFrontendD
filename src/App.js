1a39d8daeec33f8612d6fe81add861eb3cab1657
Pinaki <pinakiprasadrajkumar@gmail.com>
2025-02-03 15:40:04 +0530

    react helmet 2

--- src/App.js
+++ src/App.js
@@ -65,100 +65,113 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <ToastContainer />
-      <Router>
-        <Routes>
-          {/* Route for the base URL */}
-          <Route
-            path="/*"
-            element={<LandingPage handleLogin={handleLogin} />}
-          />
+    <>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>My React App</title>
+          <meta name="description" content="This is an awesome web app!" />
 
-          {/* Login and Register Routes */}
-          <Route
-            path="/login"
-            element={<LoginPage handleLogin={handleLogin} />}
+          {/* Open Graph Meta Tags */}
+          <meta property="og:title" content="My React App" />
+          <meta
+            property="og:description"
+            content="This is an awesome web app!"
           />
-          <Route
-            path="/register"
-            element={<RegisterPage handleLogin={handleLogin} />}
-          />
+          <meta property="og:image" content="https://alumnify.in/image.jpg" />
+          <meta property="og:url" content={window.location.href} />
+          <meta property="og:type" content="website" />
 
-          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-          <Route
-            path="/reset-password/:userId/:token"
-            element={<PasswordReset />}
+          {/* Twitter Card for social sharing */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="My React App" />
+          <meta
+            name="twitter:description"
+            content="This is an awesome web app!"
           />
+          <meta name="twitter:image" content="https://alumnify.in/image.jpg" />
+        </Helmet>
+      <div className="App">
+        <ToastContainer />
+        <Router>
+          <Routes>
+            {/* Route for the base URL */}
+            <Route
+              path="/*"
+              element={<LandingPage handleLogin={handleLogin} />}
+            />
 
-          {isLoggedIn && (
-            <>
-              {/* Home route for logged-in users */}
-              <Route
-                path="/home/*"
-                element={<Dashboard handleLogout={handleLogout} />}
-              />
-              <Route
-                path="/groups/:_id/invite/*"
-                element={
-                  <div
-                    style={{
-                      width: "100%",
-                      display: "flex",
-                      flexDirection: "row",
-                    }}
-                  >
-                    <LeftSidebar />
-                    <div style={{ marginLeft: "20%", width: "80%" }}>
-                      <TopBar handleLogout={handleLogout} />
-                      <JoinGroup />
+            {/* Login and Register Routes */}
+            <Route
+              path="/login"
+              element={<LoginPage handleLogin={handleLogin} />}
+            />
+            <Route
+              path="/register"
+              element={<RegisterPage handleLogin={handleLogin} />}
+            />
+
+            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+            <Route
+              path="/reset-password/:userId/:token"
+              element={<PasswordReset />}
+            />
+
+            {isLoggedIn && (
+              <>
+                {/* Home route for logged-in users */}
+                <Route
+                  path="/home/*"
+                  element={<Dashboard handleLogout={handleLogout} />}
+                />
+                <Route
+                  path="/groups/:_id/invite/*"
+                  element={
+                    <div
+                      style={{
+                        width: "100%",
+                        display: "flex",
+                        flexDirection: "row",
+                      }}
+                    >
+                      <LeftSidebar />
+                      <div style={{ marginLeft: "20%", width: "80%" }}>
+                        <TopBar handleLogout={handleLogout} />
+                        <JoinGroup />
+                      </div>
                     </div>
-                  </div>
-                }
-              />
-              <Route
-                path="/events/:_id/*"
-                element={
-                  <div
-                    style={{
-                      width: "100%",
-                      display: "flex",
-                      flexDirection: "row",
-                    }}
-                  >
-                    <LeftSidebar />
-                    <div style={{ marginLeft: "20%", width: "80%" }}>
-                      <TopBar handleLogout={handleLogout} />
-                      <Events />
+                  }
+                />
+                <Route
+                  path="/events/:_id/*"
+                  element={
+                    <div
+                      style={{
+                        width: "100%",
+                        display: "flex",
+                        flexDirection: "row",
+                      }}
+                    >
+                      <LeftSidebar />
+                      <div style={{ marginLeft: "20%", width: "80%" }}>
+                        <TopBar handleLogout={handleLogout} />
+                        <Events />
+                      </div>
                     </div>
-                  </div>
-                }
-              />
-            </>
-          )}
+                  }
+                />
+              </>
+            )}
 
-          {/* Catch-all route to redirect to LandingPage */}
-          <Route path="*" element={<LandingPage handleLogin={handleLogin} />} />
-        </Routes>
-      </Router>
-      <Helmet>
-        <title>My React App</title>
-
-        {/* Open Graph Meta Tags */}
-        <meta property="og:title" content="My React App" />
-        <meta property="og:description" content="This is an awesome web app!" />
-        <meta property="og:image" content="https://alumnify.in/image.jpg" />
-        <meta property="og:url" content="https://alumnify.in" />
-        <meta property="og:type" content="website" />
-
-        {/* Twitter Card for better social sharing */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="My React App" />
-        <meta name="twitter:description" content="This is an awesome web app!" />
-        <meta name="twitter:image" content="https://alumnify.in/image.jpg" />
-      </Helmet>
-    </div>
-    
+            {/* Catch-all route to redirect to LandingPage */}
+            <Route
+              path="*"
+              element={<LandingPage handleLogin={handleLogin} />}
+            />
+          </Routes>
+        </Router>
+      </div>
+    </>
   );
 }
 
