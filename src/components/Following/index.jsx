6c29d6a4a88002a953772a092f2b42d8c77b8e5d
Sahil Gagan <51903665+sahil2k19@users.noreply.github.com>
2025-04-10 12:23:05 +0530

    change following page

--- src/components/Following/index.jsx
+++ src/components/Following/index.jsx
@@ -13,13 +13,13 @@ export const Following = () => {
   const [members, setMembers] = useState([]);
   const [cookie, setCookie] = useCookies(['access_token']);
   const { id } = useParams();
-  const [loading, setLoading]= useState(true);
+  const [loading, setLoading] = useState(true);
   const LIMIT = 6;
   const [totalFollowing, setTotalFollowing] = useState(0);
   const activePage = useRef(1);
-  const profile = useSelector((state)=> state.profile);
-  const member = useSelector((state)=> state.member);
-  
+  const profile = useSelector((state) => state.profile);
+  const member = useSelector((state) => state.member);
+
   const fetchMembers = async (page) => {
     try {
       const response = await fetch(`${baseUrl}/alumni/${id}/following?page=${page}&size=${LIMIT}`);
@@ -41,34 +41,47 @@ export const Following = () => {
     fetchMembers(activePage.current);
   }, []);
 
-  const updateFollowing = () =>{
+  const updateFollowing = () => {
     activePage.current++;
     fetchMembers(activePage.current);
   }
 
   return (
-    <div style={{ width: '100%', marginTop:'20px'}}>
-      <PageTitle title={title} icon={icon} />
-      {loading && (
-        <div style={{textAlign: 'center'}}> Loading...</div>
-      )}
-      {members.length > 0 && !loading && (
-        <div style={{marginTop: '15px',display: 'flex', gap: '30px', flexWrap: 'wrap',margin: '5%'}}>
-          {members.map((member) => (
-            <Profilecard key={member._id} member={member} name='follow'/>
-          ))}
+    <div style={{ width: '100%', padding: '0% 5%' }}>
+      <div  style={{
+        paddingBottom: '2em',
+        display: 'flex',
+        flexDirection: 'column',
+        paddingTop: '25px',
+      }}>
+        <div className='bg-[#cef3df] p-4 rounded-lg mb-3 '>
+          <h2 className='text-[#136175] mb-2 text-3xl md:text-4xl font-bold'>Following</h2>
+          <p className='text-base md:text-lg text-[#136175]' >
+            {`View the profiles you’re following and stay connected.`}
+          </p>
         </div>
-      )}
-      {members.length === 0 && !loading && (
-        <div style={{ textAlign: 'center' }}>No Following</div>
-      )}
-      {activePage.current < totalFollowing / LIMIT && (
-        <div style={{textAlign: 'center', marginTop: '20px'}}>
-          <button className="load-more-button" onClick={updateFollowing}>
-            Load More
-          </button>
-        </div>
-      )}
+          {/* <PageTitle title={title} icon={icon} /> */}
+          {loading && (
+            <div style={{ textAlign: 'center' }}> Loading...</div>
+          )}
+          {members.length > 0 && !loading && (
+            <div style={{ marginTop: '15px', display: 'flex', gap: '30px', flexWrap: 'wrap', margin: '5%' }}>
+              {members.map((member) => (
+                <Profilecard key={member._id} member={member} name='follow' />
+              ))}
+            </div>
+          )}
+          {members.length === 0 && !loading && (
+            <div style={{ textAlign: 'center' }}>No Following</div>
+          )}
+          {activePage.current < totalFollowing / LIMIT && (
+            <div style={{ textAlign: 'center', marginTop: '20px' }}>
+              <button className="load-more-button" onClick={updateFollowing}>
+                Load More
+              </button>
+            </div>
+          )}
+      </div>
     </div>
   )
 }
